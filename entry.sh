5cd26d186ec06c6d0a36cd703807a5ef45478421
Suika <31232410+suikascarlet@users.noreply.github.com>
2021-01-12 16:51:25 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
-
-USERNAME="remila"
-PASSWORD="sakuya"
-
-set -x
-set -e
-
-if [ -z "${USERNAME}" ]; then
-  echo "You have to specify the -e USERNAME=... argument"
-  exit
-fi
-if [ -z "${PASSWORD}" ]; then
-  echo "You have to specify the -e PASSWORD=... argument"
-  exit
-fi
-
-htpasswd -cb /etc/squid/passwd "${USERNAME}" "${PASSWORD}"
-
-( socat tcp-listen:$PORT,reuseaddr,fork tcp:localhost:3129 ) & exec $(which squid) -NYCd 1
+cd /gostbin
+./gost -L=$GOST_RUN_SERVER:$PORT
